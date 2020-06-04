9e9aa5a0ffae6e72005c597b93a2fda8e6dc5b56
Andy Loftus <aloftus@gmail.com>
2020-06-04 15:58:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# NCSA Puppet Profiles - Additional YumRepos
+ 
+Collect repo definitions from hiera and ensure they are created.
+ 
+## Dependencies
+None
+ 
+## Reference
+ 
+### class profile_additional_yumrepos (
+-    Hash $repo_list,
+
+[REFERENCE.md](REFERENCE.md)
---
+++ manifests/init.pp
@@ -0,0 +1,30 @@
+# @summary Install additional yumrepos (specified in Hiera)
+#
+# Install additional yumrepos (specified in Hiera)
+#
+# @param repo_list Hash of (yumrepo configs) to create
+#   Hash key will be used as the yumrepo 'resource_title'
+#   Hash value must be a Hash of valid yumrepo attributes
+#     If 'ensure' is not provided, it will default to 'present'
+#     If 'enabled' is not provided, it will default to true
+#
+# @example
+#   baseline_cfg::additional_yumrepos:repo_list:
+#     'rsyslog-v8':
+#       baseurl: http://rpms.adiscon.com/v8-stable/epel-$releasever/$basearch
+#       descr: rsyslog official repo
+#       gpgcheck: true
+#       gpgkey: http://rpms.adiscon.com/RPM-GPG-KEY-Adiscon
+#
+# @example
+#   include baseline_cfg::additional_yumrepos
+class profile_additional_yumrepos (
+    Hash $repo_list,
+) {
+    $defaults = {
+      ensure  => present,
+      enabled => true,
+    }
+
+    ensure_resources( 'yumrepo', $repo_list, $defaults )
+}
